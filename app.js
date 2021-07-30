72cad9a1d8aea5e5bf73c7af24fe714e586a04af
JuanGonzalez1889 <jgonzalez@decreditos.com>
2021-07-30 00:00:48 -0300

    probando html

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require("path")
+
+app.listen(3000,() => {
+    console.log("servidor corriendo OK")
+});
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
