765570a84b94e72537a463ade15afdb27a038d2d
JuanGonzalez1889 <jgonzalez@decreditos.com>
2021-08-18 01:04:57 -0300

    subo hover actualizado

--- app.js
+++ app.js
@@ -11,3 +11,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+
