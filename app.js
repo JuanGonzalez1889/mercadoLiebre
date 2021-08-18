ef19019dd516e9daf291782d787c9ab0b7214700
JuanGonzalez1889 <jgonzalez@decreditos.com>
2021-08-18 17:52:32 -0300

    agregando login

--- app.js
+++ app.js
@@ -15,4 +15,7 @@ app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "views/register.html"));
 });
 
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
 
